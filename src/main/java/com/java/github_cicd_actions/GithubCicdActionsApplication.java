dba279334daf622d1a4c62dbe0af71bcc25634f2
Mandeep Singh <smand@Mandeep-PC>
2025-07-25 21:21:02 +0530

    code

---
+++ src/main/java/com/java/github_cicd_actions/GithubCicdActionsApplication.java
@@ -0,0 +1,27 @@
+package com.java.github_cicd_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+	@GetMapping("/welcome")
+	public  String  welcome(){
+		return "Welcome to the club baby";
+	}
+//	echo "# github-actions-example" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/mandeep-creater/github-actions-example.git
+//	git push -u origin main
+	public static void main(String[] args) {
+
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
